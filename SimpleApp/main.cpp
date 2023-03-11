0a09ff7137f125e5c3efb811decba1635b0a5059
Alkulator <aleksanderf103@hotmail.com>
2023-03-11 22:02:32 +0100

    dadsadsad

--- SimpleApp/main.cpp
+++ SimpleApp/main.cpp
@@ -12,6 +12,7 @@ int main() {
     for (int i = 0; i < 10; ++i){
         std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
         std::cout << "i have added this line using wim in the mac terminal" << std::endl;
+        std::cout << "i have added this line to " << std::endl;
     }
     
     
