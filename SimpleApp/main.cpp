b20fb541bd72e54ed8ba85834f60c8a00c57ea05
Akulator <77936184+Alkulator@users.noreply.github.com>
2023-03-11 21:49:23 +0100

    fafdafafda

--- SimpleApp/main.cpp
+++ SimpleApp/main.cpp
@@ -7,8 +7,12 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    std::cout << "i want to learn how to use github to my advantage " << std::endl;
+    for (int i = 0; i < 10; ++i){
+        std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
+        std::cout << "i have added this line using wim in the mac terminal" << std::endl;
+    }
+    
+    
 }
