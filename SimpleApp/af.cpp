e87fd6a66bd5edad0ef506cf6a5755af8707a62b
Alkulator <aleksanderf103@hotmail.com>
2023-03-11 21:55:29 +0100

    dffdaddfafd

--- SimpleApp/af.cpp
+++ SimpleApp/af.cpp
@@ -11,7 +11,8 @@ int main() {
     std::cout << "i want to learn how to use github to my advantage " << std::endl;
     for (int i = 0; i < 10; ++i){
         std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
-        std::cout << "i have added this line using wim in the mac terminal" << std::endl;
+    std:.cout << "i also added this line " << std::endl;
+    std::cout << "i have added this line using wim in the mac terminal" << std::endl;
     }
     
     
