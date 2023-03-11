9f0376b8bc1fb32a5d7adcfd19fdb7fb23a6f1d4
Akulator <77936184+Alkulator@users.noreply.github.com>
2023-03-11 21:46:05 +0100

    I have placed another file in the project for me to work on. greeting from af

---
+++ SimpleApp/af.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SimpleApp
+//
+//  Created by Aleksander Forsland on 11/03/2023.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "i want to learn how to use github to my advantage " << std::endl;
+    for (int i = 0; i < 10; ++i){
+        std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
+        std::cout << "i have added this line using wim in the mac terminal" << std::endl;
+    }
+    
+    
+}
