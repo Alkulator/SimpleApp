1016c588228ee4a23275678a9ae145b7086e148a
Alkulator <aleksanderf103@hotmail.com>
2023-03-13 18:25:04 +0100

    dfafdaf

--- SimpleApp/main.cpp
+++ SimpleApp/main.cpp
@@ -1,4 +1,3 @@
-//
 //  main.cpp
 //  SimpleApp
 //
@@ -12,7 +11,7 @@ int main() {
     for (int i = 0; i < 10; ++i){
         std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
         std::cout << "i have added this line using wim in the mac terminal" << std::endl;
-        std::cout << "i have added this line to " << std::endl;
+       			 std::cout << "i have added this line to " << std::endl;
     }
     
     
