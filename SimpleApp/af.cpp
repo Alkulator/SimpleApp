14db8e39835c81ca85b10c2780b382e044b8c78d
Alkulator <aleksanderf103@hotmail.com>
2023-03-13 19:09:16 +0100

    i have made a commit AF

--- SimpleApp/af.cpp
+++ SimpleApp/af.cpp
@@ -1,19 +1,8 @@
 //
-//  main.cpp
+//  Af.cpp
 //  SimpleApp
 //
-//  Created by Aleksander Forsland on 11/03/2023.
+//  Created by Aleksander Forsland on 13/03/2023.
 //
 
-#include <iostream>
-
-int main() {
-    std::cout << "i want to learn how to use github to my advantage " << std::endl;
-    for (int i = 0; i < 10; ++i){
-        std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
-    std:.cout << "i also added this line " << std::endl;
-    std::cout << "i have added this line using wim in the mac terminal" << std::endl;
-    }
-    
-    
-}
+#include "Af.hpp"
--- SimpleApp/main.cpp
+++ SimpleApp/main.cpp
@@ -7,11 +7,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "i want to learn how to use github to my advantage " << std::endl;
-    for (int i = 0; i < 10; ++i){
-        std::cout << "the loop have run : " << i + 1  << "times" << std::endl;
-        std::cout << "i have added this line using wim in the mac terminal" << std::endl;
-       			 std::cout << "i have added this line to " << std::endl;
+    for (int i = 0; i <= 10; ++i){
+        std::cout << "hello world" << i << std::endl;
     }
     
     
